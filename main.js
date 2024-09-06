b17477ac4be659dc9171709babb6212a50751157
Wolny Michał <M.Wolny@bossa.pl>
2024-09-07 01:30:26 +0200

    new changes

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-import { setupScroll } from './scroll.js'
 import './fullpage.js'
+import 'fslightbox'
 
-setupScroll(document.querySelectorAll(".js-scroll"))
+
+new fullpage('#fullpage', {
+    menu: '.werpol-menu',
+    anchors: ['intro', 'about', 'offer', 'equipment', 'implementations', 'images', 'movies', 'references', 'contact'],
+    
+    // Navigation for horizontal slides
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom', // Default
+
+    controlArrows: false,
+
+    normalScrollElements: '.werpol-references-preview-companies',
+
+    // Get your license at https://alvarotrigo.com/fullPage/pricing
+    licenseKey: 'xxxxxxxxxxxxxxxxxxxxxxxxx'
+});
